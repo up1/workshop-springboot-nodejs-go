55d4abdfeab6f1b1dafb2a2b219c50744130005e
up1 <somkiat.p@gmail.com>
2025-04-02 15:55:17 +0700

    Add first test with spring boot test

---
+++ workshop/day01/src/test/java/com/example/day01/user/UserControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.day01.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class UserControllerTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void getUserById() {
+        UserResponse actualResult
+                = restTemplate.getForObject("/user/1", UserResponse.class);
+        assertEquals(1, actualResult.getId());
+        assertEquals("Somkiat", actualResult.getFirst_name());
+        assertEquals("somkiat@xx.com", actualResult.getEmail());
+
+    }
+}
