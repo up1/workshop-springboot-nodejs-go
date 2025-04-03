3fd17219e7e97138ae7088e03383c469e1cece18
up1 <somkiat.p@gmail.com>
2025-04-03 15:52:47 +0700

    Add prom

--- workshop/nodejs-api/index.js
+++ workshop/nodejs-api/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const promBundle = require("express-prom-bundle");
+const metricsMiddleware = promBundle({includeMethod: true});
+app.use(metricsMiddleware);
 app.get('/hello', (req, res) => {
-    res.json({ message: 'Hello World!' })
+    res.json({ message: 'Hello World......' })
 })
 
 app.listen(port, () => {
