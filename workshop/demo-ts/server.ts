7d9702fa016bcb04741f019adb7b08f7b9c511d8
up1 <somkiat.p@gmail.com>
2025-04-03 23:28:58 +0700

    Demo with structured log

--- workshop/demo-ts/server.ts
+++ workshop/demo-ts/server.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import { Product } from "./product";
 
+import logger from "pino-http";
+
 export class Server {
   private app: Application;
   private port: number;
@@ -9,6 +11,7 @@ export class Server {
     this.app = express();
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(logger());
     this.port = port;
     this.setupRoutes();
   }
@@ -19,6 +22,7 @@ export class Server {
   }
 
   private handleRoot(req: Request, res: Response): void {
+    req.log.info({key: "value"}, "Demo structured log");
     res.status(200).json({ message: "Hello World!" });
   }
 
