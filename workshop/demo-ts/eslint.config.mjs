0863993d3d7bc64946f5f4cd69210979f64e8ee8
up1 <somkiat.p@gmail.com>
2025-04-03 23:14:33 +0700

    Add more rules

--- workshop/demo-ts/eslint.config.mjs
+++ workshop/demo-ts/eslint.config.mjs
@@ -4,6 +4,17 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    // add any additional rules or overrides here
+    {
+        rules: {
+            'no-console': 'warn',
+            'no-unused-vars': 'warn',
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            // Add more rules as needed
+        },
+    }
+  );
