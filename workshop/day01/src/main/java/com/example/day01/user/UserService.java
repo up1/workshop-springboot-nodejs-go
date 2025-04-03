153403373d28406b659ccbc14680e3e2c3e9401a
up1 <somkiat.p@gmail.com>
2025-04-03 12:42:23 +0700

    Working with tx

--- workshop/day01/src/main/java/com/example/day01/user/UserService.java
+++ workshop/day01/src/main/java/com/example/day01/user/UserService.java
@@ -1,11 +1,13 @@
 package com.example.day01.user;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
@@ -15,6 +17,12 @@ public class UserService {
     }
 
     public UserResponse getData(int id) {
+        userRepository.deleteAll();
+        userRepository.save(new MyUser());
+        userRepository.save(new MyUser());
+
+        // Call external service timeout 10 secs
+
         Optional<MyUser> result = userRepository.findById(id);
         if(result.isEmpty()) {
             throw new UserNotFoundException(String.valueOf(id));
