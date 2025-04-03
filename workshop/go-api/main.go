58ff6f0aa38a5b746ac93bdf64061e31e96df3db
up1 <somkiat.p@gmail.com>
2025-04-03 16:29:26 +0700

    Add prometheus

--- workshop/go-api/main.go
+++ workshop/go-api/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(echoprometheus.NewMiddleware("myapp"))   // adds middleware to gather metrics
+	e.GET("/metrics", echoprometheus.NewHandler()) // adds route to serve gathered metrics
+
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
 	})
