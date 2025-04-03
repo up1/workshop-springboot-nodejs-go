3f4212e4595698b9c59c2d1de7b7ef46bbac91bb
up1 <somkiat.p@gmail.com>
2025-04-03 16:21:50 +0700

    Demo with go

---
+++ workshop/go-api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
