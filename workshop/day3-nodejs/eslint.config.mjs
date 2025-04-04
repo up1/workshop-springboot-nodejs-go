ef248acc0edc93fd87c488d907c1c7d363d2f510
up1 <somkiat.p@gmail.com>
2025-04-04 10:12:54 +0700

    Add eslint

---
+++ workshop/day3-nodejs/eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    // add any additional rules or overrides here
+    {
+        rules: {
+            'no-console': 'warn',
+            'no-unused-vars': 'warn',
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            // Add more rules as needed
+        },
+    }
+  );
--- workshop/day3-nodejs/server.ts
+++ workshop/day3-nodejs/server.ts
@@ -1,11 +1,32 @@
-import express, { Request, Response } from "express";
-const app = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express';
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+class Server {
+  private app: Application;
+  private port: number;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  constructor(port = 3000) {
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.port = port;
+    this.setupRoutes();
+  }
+
+  private setupRoutes(): void {
+    this.app.get('/', this.handleRoot.bind(this));
+  }
+
+  private handleRoot(req: Request, res: Response): void {
+    res.status(200).json({ message: 'Hello World!' });
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Example app listening on port ${this.port}`);
+    });
+  }
+}
+
+// Create and start the server
+const server = new Server();
+server.start();
